4467af40e71a9f0423e38b938ef0473576e04ade
Tina2010 <t.rittmueller@gmx.de>
2021-11-12 15:34:14 +0100

    [feat] adding tests for Event/NumberOfEvents/EventList/CitySearch

---
+++ src/__tests__/Event.test.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { mockData } from "../mock-data";
+import Event from '../Event';
+
+describe('<Event /> component', () => {
+    let EventWrapper;
+    let event = mockData[0];
+    beforeAll(() => {
+        EventWrapper = shallow(<Event event={event} />);
+    });
+  
+    test('render summary', () => {
+      expect(EventWrapper.find('.summary')).toHaveLength(1);
+    });
+  
+    test('render location', () => {
+      expect(EventWrapper.find('.location')).toHaveLength(1);
+    });
+
+    test('render startDatetime', () => {
+      expect(EventWrapper.find('.startDatetime')).toHaveLength(1);
+    });
+  
+    test('render startTimezone', () => {
+      expect(EventWrapper.find('.startTimezone')).toHaveLength(1);
+    });
+
+    test('render description', () => {
+      expect(EventWrapper.find('.description')).toHaveLength(1);
+    });
+
+    test('render details button', () => {
+        expect(EventWrapper.find('.detailsButton')).toHaveLength(1);
+      });
+
+    test('open details section via onClick on details button', () => {
+        EventWrapper.find('.detailsButton').at(0).simulate('click');
+        expect(EventWrapper.state('detailsShown')).toBe(true);
+    });
+});
---
+++ src/__tests__/NumberOfEvents.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import NumberOfEvents from '../NumberOfEvents';
+
+describe('<NumberOfEvents /> component', () => {
+  let NumberOfEventsWrapper;
+   beforeAll(() => {
+    NumberOfEventsWrapper = shallow(<NumberOfEvents/>);
+  });
+    
+  test('render textBox', () => {
+    expect(NumberOfEventsWrapper.find('.numberOfEvents')).toHaveLength(1);
+  });
+
+  test('return amount of events, when amount is changed manually', () => {
+    NumberOfEventsWrapper.setState({
+        manualAmountEvents: 2
+    })
+    const eventObject = { target: {value: 1}};
+    NumberOfEventsWrapper.find('.amountEventsOnePage').simulate('change', eventObject);
+    expect(NumberOfEventsWrapper.state('manualAmountEvents')).toBe(1);
+  });
+
+});
