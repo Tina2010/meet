a3d938fecbb94cc8ce4e0b7d0c4f40fffb63da32
Tina2010 <t.rittmueller@gmx.de>
2021-11-17 16:42:19 +0100

    [feat] adding endToEnd tests - Filter events by city

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -1,13 +1,13 @@
 import puppeteer from 'puppeteer';
 
-describe('show/hide an event details', () => {
+/* describe('show/hide an event details', () => {
     let browser;
     let page;
     jest.setTimeout(900 * 1000);
   
     beforeAll(async () => {
       browser = await puppeteer.launch({
-        headless: false,
+        headless: true,
         slowMo: 250, // slow down by 250ms
        ignoreDefaultArgs: ['--disable-extensions'] // ignores default setting that causes timeout errors
       }); //only add headless mode after the entire test-file ran succesfully
@@ -36,4 +36,42 @@ describe('show/hide an event details', () => {
     const eventDetails = await page.$('.eventCard .eventDetails.active');
     expect(eventDetails).toBeNull();
   });
+}); */
+
+describe('Filter events by city', () => {
+    let browser;
+    let page;
+    jest.setTimeout(900 * 1000);
+  
+    beforeAll(async () => {
+      browser = await puppeteer.launch({
+        headless: true,
+        slowMo: 250, // slow down by 250ms
+       ignoreDefaultArgs: ['--disable-extensions'] // ignores default setting that causes timeout errors
+      }); //only add headless mode after the entire test-file ran succesfully
+      page = await browser.newPage();
+      await page.goto('http://localhost:3000/');
+      await page.waitForSelector('.eventCard');
+    });
+  
+    afterAll(() => {
+      browser.close();
+    });
+
+    test('When user hasn’t searched for a city, show upcoming events from all cities.', () => {
+        expect('.event').toHaveLength(6);
+    });
+
+    test('User should see a list of suggestions when they search for a city.', async () => {
+        await page.type('.city', 'Berlin');
+        expect('li').toHaveLength(2);
+    });
+
+    test('User can select a city from the suggested list.', async () => {
+        await page.type('.city', 'Berlin');
+        await page.click('.suggestions', 'Berlin, Germany');
+        await page.evaluate(
+            () => document.querySelector(".city").value === "Berlin, Germany"
+          );       
+    });
 });
